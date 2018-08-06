5157fbaa71d8ffd79dea2c3398e3795b0aec72a1
miracletjf <1398988546@qq.com>
2018-08-06 23:54:22 +0800

    获取外链

--- src/qiniuDemo.js
+++ src/qiniuDemo.js
@@ -16,7 +16,7 @@ var uploader = Qiniu.uploader({
   // Ajax请求downToken的Url，私有空间时使用,JS-SDK 将向该地址POST文件的key和domain,服务端返回的JSON必须包含`url`字段，`url`值为该文件的下载地址
   // unique_names: true,              // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
   // save_key: true,                  // 默认 false。若在服务端生成 uptoken 的上传策略中指定了 `save_key`，则开启，SDK在前端将不对key进行任何处理
-  domain: 'http://pc5zw9rcj.bkt.clouddn.com', // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
+  domain: 'pc5zw9rcj.bkt.clouddn.com', // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
   container: 'uploadBox', // 上传区域 DOM ID，默认是 browser_button 的父元素，
   max_file_size: '15mb', // 最大文件体积限制
   // flash_swf_url: 'path/of/plupload/Moxie.swf',  //引入 flash,相对路径
@@ -61,9 +61,11 @@ var uploader = Qiniu.uploader({
       //  }
       // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
 
-      // var domain = up.getOption('domain');
-      // var res = parseJSON(info.response);
-      // var sourceLink = domain + res.key; 获取上传成功后的文件的Url
+      var domain = up.getOption('domain');
+      console.log(info);
+      var res = JSON.parse(info.response);
+      var sourceLink = 'http://'+domain + '/' + res.key; //获取上传成功后的文件的Url
+      console.log(sourceLink);
     },
     'Error': function (up, err, errTip) {
       //上传出错时,处理相关的事情
