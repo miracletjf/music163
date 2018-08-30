0df591581f27b73ea46b124821b69c3d960045d1
miracletjf <1398988546@qq.com>
2018-08-31 01:07:42 +0800

    complete song list , get song list

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -2,25 +2,60 @@
   let view = {
     el: '#songList',
     template: `<li>
-      <a href="" class="item">
-        <h3>Summer Sunshine</h3>
+      <a href="" class="item" data-song-id="{{id}}">
+        <h3>{{name}}</h3>
         <p class="text">
           <svg class="icon icon-sq" aria-hidden="true"> <use xlink:href="#icon-sq"></use> </svg>
-          时光街乐队 - Summer Sunshine
+          {{author}}
         </p>
         <i> <svg class="icon icon-bofang" aria-hidden="true"> <use xlink:href="#icon-bofang"></use> </svg> </i>
       </a>
-    </li>`
+    </li>`,
+    init(){
+      this.$el = $(this.el);
+    },
+    render(data){
+      let {songs,props} = data;
+      let songsHtml = songs.reduce((str,song)=>{
+        let template = this.template;
+        props.map(item=>{
+          console.log(item,song[item]);
+          template = template.replace(`{{${item}}}`,song[item]);
+        });
+        console.log(template);
+        return str + template;
+      },'');
+
+      this.$el.append(songsHtml);
+    }
   }
 
   let model = {
-    data:{}
+    data:{ 
+      props: ['id','name','author','url']
+    },
+    featchAll(){
+      var query = new AV.Query('Song');
+      return query.find().then( songs => {
+        this.data.songs = songs.map(song=>{
+          return {id:song.id,...song.attributes}
+        });
+      })
+    }
+
   }
 
   let controller = {
     init(view,model){
       this.view = view;
       this.model = model;
+      this.view.init();
+      this.getAllSongs();
+    },
+    getAllSongs(){
+      this.model.featchAll().then(()=>{
+        this.view.render(this.model.data);
+      })
     }
   }
 
